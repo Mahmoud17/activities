1d0d4b76b3d5662b83bc2be94ee3aa914d652689
Mahmoud Ayoub <mahmoud30ayoub@gmail.com>
2024-07-07 20:33:29 +0300

    Add project files.

---
+++ activities/Controllers/ActivitiesController.cs
@@ -0,0 +1,33 @@
+﻿using System.Runtime.InteropServices;
+using domain;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using persistance;
+
+namespace activities.Controllers
+{
+	[Route("api/[controller]")]
+	[ApiController]
+	public class ActivitiesController : ControllerBase
+	{
+		public ActivitiesController(DataContext dataContext)
+		{
+			DataContext = dataContext;
+		}
+
+		public DataContext DataContext { get; }
+
+		[HttpGet]
+		public async Task<ActionResult<List<Activity>>> GetActivities()
+		{
+			return await DataContext.Set<Activity>().ToListAsync();
+		}
+
+		[HttpGet("{id}")]
+		public async Task<ActionResult<Activity>> GetActivity(Guid id)
+		{
+			return await DataContext.Activities.FindAsync(id);
+		}
+	}
+}
---
+++ persistance/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace persistance
+{
+	public class DataContext : DbContext
+	{
+		public DataContext(DbContextOptions options) : base(options)
+		{
+		}
+
+		public DbSet<Activity> Activities { get; set; }
+	}
+}
